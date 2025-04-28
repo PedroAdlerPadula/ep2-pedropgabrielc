f97736bc26fcc3668a12c0bd6ce4b2d26ad998a7
Pedro Adler Padula <pedroap3@al.insper.edu.br>
2025-04-28 09:04:46 -0300

    Exs 2 + arquivo de teste

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,14 @@ def rolar_dados(numero):
         num = random.randint(1, 6)
         lista_final.append(num)
     return lista_final
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dados_rolados2 = []
+    lista_final = [dados_rolados2, dados_no_estoque]
+    for i in range(len(dados_rolados)):
+        if i == dado_para_guardar:
+            dados_no_estoque.append(dados_rolados[i])
+            dados_rolados[i] not in dados_rolados
+        else:
+            dados_rolados2.append(dados_rolados[i])
+    return lista_final
