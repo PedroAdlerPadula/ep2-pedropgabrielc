7250c67de16079b0bea1f3cc670686646b1a7aa2
Gabriel Correa <gabrielcaccorrea@gmail.com>
2025-05-06 14:31:34 -0300

    ex 13 ajuste

--- programa.py
+++ programa.py
@@ -1,102 +1,101 @@
-import funcoes
+from funcoes import *
 
-cartela_de_pontos = {
+armazena_pontos  = {
     'regra_simples': {
-        '1': -1,
-        '2': -1,
-        '3': -1,
-        '4': -1,
-        '5': -1,
-        '6': -1
+        1: -1,
+        2: -1,
+        3: -1,
+        4: -1,
+        5: -1, 
+        6: -1,
     },
-    'regra_avancada': {
+    'regra_avancada' : {
         'sem_combinacao': -1,
         'quadra': -1,
         'full_house': -1,
         'sequencia_baixa': -1,
         'sequencia_alta': -1,
-        'cinco_iguais': -1
+        'cinco_iguais': -1,
+        
     }
 }
 
-jogada = 0
+contagem = 0 
 
-funcoes.imprime_cartela(cartela_de_pontos)
+imprime_cartela(armazena_pontos)
 
-while jogada < 12:
-    dados = funcoes.rolar_dados(5)
-    guardados = []
-    jogando = True
-    rolar_novamente = 0
-
+while contagem < 12: 
+    dados = rolar_dados(5)
+    dados_guardados = []
+    jogando = True 
+    rolar_novamente = 0 
     while jogando:
-        print("Dados rolados: {}".format(dados))
-        print("Dados guardados: {}".format(guardados))
+        print("Dados rolados: {} ".format(dados))
+        print("Dados guardados: {} ".format(dados_guardados))
         print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
-        acao = input(">")
+        entrada = input(">")
 
-        while acao != "0" and acao != "1" and acao != "2" and acao != "3" and acao != "4":
-            print("Opção inválida. Tente novamente.")
-            acao = input(">")
+        while entrada != "0" and entrada != "1" and entrada != "2" and entrada != "3" and entrada != "4":
+            print("Opção invalida. Tente novamente.")
+            entrada = input(">")
 
-        if acao == "0":
+        if entrada == "0":
             print("Digite a combinação desejada:")
             while jogando:
                 combinacao = input(">")
-                if combinacao == "1" or combinacao == "2" or combinacao == "3" or combinacao == "4" or combinacao == "5" or combinacao == "6":
-                    if cartela_de_pontos["regra_simples"][int(combinacao)] != -1:
+
+                if combinacao == "1" or combinacao == "2" or combinacao == "3" or combinacao == "4" or combinacao == "5"or combinacao == "6":
+                    if armazena_pontos["regra_simples"][int(combinacao)] != -1:
                         print("Essa combinação já foi utilizada.")
                     else:
-                        lista_jogada = guardados + dados
-                        cartela_de_pontos = funcoes.faz_jogada(lista_jogada, combinacao, cartela_de_pontos)
+                        lista_jogada = dados_guardados + dados
+                        armazena_pontos = faz_jogada(lista_jogada, combinacao, armazena_pontos)
                         jogando = False
                 elif combinacao == "sem_combinacao" or combinacao == "quadra" or combinacao == "full_house" or combinacao == "sequencia_baixa" or combinacao == "sequencia_alta" or combinacao == "cinco_iguais":
-                    if cartela_de_pontos["regra_avancada"][combinacao] != -1:
+                    if armazena_pontos["regra_avancada"][combinacao] != -1:
                         print("Essa combinação já foi utilizada.")
                     else:
-                        lista_jogada = guardados + dados
-                        cartela_de_pontos = funcoes.faz_jogada(lista_jogada, combinacao, cartela_de_pontos)
+                        lista_jogada = dados_guardados + dados
+                        armazena_pontos = faz_jogada(lista_jogada, combinacao, armazena_pontos)
                         jogando = False
                 else:
                     print("Combinação inválida. Tente novamente.")
 
-        elif acao == "1":
+        elif entrada == "1":
             print("Digite o índice do dado a ser guardado (0 a 4):")
-            r = int(input(">"))
-            if len(dados) > 0:
-                lista = funcoes.guardar_dado(dados, guardados, r)
+            g = int(input(">"))
+            if g < len(dados):
+                lista = guardar_dado(dados, dados_guardados, g)
                 dados = lista[0]
-                guardados = lista[1]
+                dados_guardados = lista[1]
 
-        elif acao == "2":
+        elif entrada == "2":
             print("Digite o índice do dado a ser removido (0 a 4):")
             r = int(input(">"))
-            if len(guardados) > 0:
-                lista = funcoes.remover_dado(dados, guardados, r)
+            if r < len(dados_guardados):
+                lista = remover_dado(dados, dados_guardados, r)
                 dados = lista[0]
-                guardados = lista[1]
+                dados_guardados = lista[1]
 
-        elif acao == "3":
+        elif entrada == "3":
             if rolar_novamente >= 2:
-                print("Você já usou todas as rerrolagens.")
+                print("Você usou todas as rerrolagens.")
             else:
-                dados = funcoes.rolar_dados(len(dados))
+                dados = rolar_dados(len(dados))
                 rolar_novamente += 1
 
-        elif acao == "4":
-            funcoes.imprime_cartela(cartela_de_pontos)
+        elif entrada == "4":
+            imprime_cartela(armazena_pontos)
 
-        if cartela_de_pontos["regra_simples"]["1"] != -1 and cartela_de_pontos["regra_simples"]["2"] != -1 and cartela_de_pontos["regra_simples"]["3"] != -1:
-            jogando = False
-            jogada = 12
+    if armazena_pontos["regra_simples"][1] != -1 and armazena_pontos["regra_simples"][2] != -1 and armazena_pontos["regra_simples"][3] != -1 and armazena_pontos["regra_simples"][4] != -1 and armazena_pontos["regra_simples"][5] != -1 and armazena_pontos["regra_simples"][6] != -1:
+        jogando = False
+        contagem = 12
 
-    jogada += 1
+    contagem += 1
 
-pontuacao_final = cartela_de_pontos["regra_simples"]["1"] + cartela_de_pontos["regra_simples"]["2"] + cartela_de_pontos["regra_simples"]["3"] + cartela_de_pontos["regra_simples"]["4"] + cartela_de_pontos["regra_simples"]["5"] + cartela_de_pontos["regra_simples"]["6"]
-simples = cartela_de_pontos["regra_simples"]["1"] + cartela_de_pontos["regra_simples"]["2"] + cartela_de_pontos["regra_simples"]["3"] + cartela_de_pontos["regra_simples"]["4"] + cartela_de_pontos["regra_simples"]["5"] + cartela_de_pontos["regra_simples"]["6"]
-
+pontuacao_final = armazena_pontos["regra_simples"][1] + armazena_pontos["regra_simples"][2] + armazena_pontos["regra_simples"][3] + armazena_pontos["regra_simples"][4] + armazena_pontos["regra_simples"][5] + armazena_pontos["regra_simples"][6]
+simples = armazena_pontos["regra_simples"][1] + armazena_pontos["regra_simples"][2] + armazena_pontos["regra_simples"][3] + armazena_pontos["regra_simples"][4] + armazena_pontos["regra_simples"][5] + armazena_pontos["regra_simples"][6]
 if simples >= 63:
     pontuacao_final = pontuacao_final + 35
-
-funcoes.imprime_cartela(cartela_de_pontos)
+imprime_cartela(armazena_pontos)
 print("Pontuação total: {}".format(pontuacao_final))
